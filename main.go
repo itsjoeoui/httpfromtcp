a4821ab45d86b83854c16d27a908dc38bce80a98
Joey Yu <joey@jyu.dev>
2025-08-23 22:15:28 -0400

    read and print messages.txt

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	fd, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("failed to open %s: %s\n", inputFilePath, err)
+		panic(err)
+	}
+	defer func() {
+		if err := fd.Close(); err != nil {
+			log.Fatalf("failed to close %s: %s\n", inputFilePath, err)
+			panic(err)
+		}
+	}()
+
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := fd.Read(buffer)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return
+			}
+
+			log.Fatalf("failed to read from %s: %s\n", inputFilePath, err)
+			panic(err)
+		}
+
+		fmt.Printf("read: %s\n", string(buffer[:n]))
+	}
 }
