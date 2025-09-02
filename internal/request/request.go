9592397a7c3d9a021a99cc7426a27eb858d0b796
Joey Yu <joey@jyu.dev>
2025-09-02 11:54:30 -0400

    parse a stream of data

--- internal/request/request.go
+++ internal/request/request.go
@@ -14,11 +14,22 @@ var (
 	supportedHTTPVersions = []string{"1.1"}
 )
 
-const crlf = "\r\n"
+const (
+	crlf       = "\r\n"
+	bufferSize = 8
+)
 
 type Request struct {
 	RequestLine RequestLine
+	ParserState ParserState
 }
+
+type ParserState string
+
+const (
+	ParserStateInitialized ParserState = "Initialized"
+	ParserStateDone        ParserState = "Done"
+)
 
 type RequestLine struct {
 	HTTPVersion   string
@@ -26,41 +37,97 @@ type RequestLine struct {
 	Method        string
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	rawBytes, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.ParserState {
+	case ParserStateInitialized:
+		requestLine, length, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if length == 0 {
+			// this means we need more data to parse the request line
+			return 0, nil
+		}
 
-	requestLine, err := parseRequestLine(rawBytes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse request line: %w", err)
+		r.RequestLine = *requestLine
+		r.ParserState = ParserStateDone
+		return length, nil
+	case ParserStateDone:
+		return 0, fmt.Errorf("request already fully parsed")
+	default:
+		return 0, fmt.Errorf("unknown parser state: %s", r.ParserState)
 	}
-
-	return &Request{
-		RequestLine: *requestLine,
-	}, nil
 }
 
-func parseRequestLine(req []byte) (*RequestLine, error) {
-	requestLine := strings.Split(string(req), crlf)[0]
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := &Request{
+		ParserState: ParserStateInitialized,
+	}
+
+	buffer := make([]byte, bufferSize)
+	readToIndex := 0
+
+	for request.ParserState != ParserStateDone {
+		if readToIndex == len(buffer) {
+			newBuffer := make([]byte, len(buffer)*2)
+			copy(newBuffer, buffer)
+			buffer = newBuffer
+		}
+
+		bytesRead, err := reader.Read(buffer[readToIndex:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				if bytesRead != 0 {
+					panic("What the hell, I thought if io.EOF is returned, bytesRead should be 0")
+				}
+				request.ParserState = ParserStateDone
+				break
+			}
+			return nil, err
+		}
+
+		readToIndex += bytesRead
+
+		parsedToIndex, err := request.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		if parsedToIndex != 0 {
+			copy(buffer, buffer[parsedToIndex:])
+			readToIndex -= parsedToIndex
+		}
+	}
+
+	return request, nil
+}
+
+func parseRequestLine(req []byte) (*RequestLine, int, error) {
+	splitReq := strings.Split(string(req), crlf)
+	if len(splitReq) <= 1 {
+		// we do not have a complete request line yet
+		return nil, 0, nil
+	}
+
+	requestLine := splitReq[0]
+
 	parts := strings.Split(requestLine, " ")
 	if len(parts) != 3 {
-		return nil, errors.New("invalid request line: incorrect number of parts")
+		return nil, 0, errors.New("invalid request line: incorrect number of parts")
 	}
 
 	if !slices.Contains(supportedHTTPMethods, parts[0]) {
-		return nil, errors.New("invalid request line: unsupported HTTP method")
+		return nil, 0, errors.New("invalid request line: unsupported HTTP method")
 	}
 
 	httpVersion := strings.TrimPrefix(parts[2], "HTTP/")
 	if !slices.Contains(supportedHTTPVersions, httpVersion) {
-		return nil, errors.New("invalid request line: unsupported HTTP version")
+		return nil, 0, errors.New("invalid request line: unsupported HTTP version")
 	}
 
 	return &RequestLine{
 		HTTPVersion:   httpVersion,
 		RequestTarget: parts[1],
 		Method:        parts[0],
-	}, nil
+	}, len(requestLine) + len(crlf), nil
 }
