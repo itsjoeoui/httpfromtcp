bd4b6e12c2e6e8ae393455f756dffd03d385c325
Joey Yu <joey@jyu.dev>
2025-09-02 17:24:26 -0400

    actually parse and print out request line

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/itsjoeoui/httpfromtcp/internal/request"
 )
 
 // `nc localhost 42069` to establish a connection
@@ -47,51 +46,13 @@ func handleConnection(conn net.Conn) {
 		log.Printf("closed connection from %s\n", conn.RemoteAddr().String())
 	}()
 
-	linesChan := getLinesChannel(conn)
-	for line := range linesChan {
-		fmt.Printf("read: %s\n", line)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		panic(err)
 	}
-}
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-
-	go func() {
-		defer close(lines)
-
-		buffer := make([]byte, 8)
-
-		var sb strings.Builder
-		for {
-			n, err := f.Read(buffer)
-			if err != nil {
-				if sb.String() != "" {
-					lines <- sb.String()
-				}
-
-				if errors.Is(err, io.EOF) {
-					break
-				}
-
-				log.Fatalf("failed to read: %s\n", err.Error())
-				panic(err)
-			}
-
-			chunks := strings.Split(string(buffer[:n]), "\n")
-
-			seen := false
-			for _, chunk := range chunks {
-				if seen {
-					lines <- sb.String()
-					sb.Reset()
-					sb.WriteString(chunk)
-				} else {
-					sb.WriteString(chunk)
-					seen = true
-				}
-			}
-		}
-	}()
-
-	return lines
+	fmt.Printf("Request line:\n")
+	fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+	fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+	fmt.Printf("- Version: %s\n", req.RequestLine.HTTPVersion)
 }
